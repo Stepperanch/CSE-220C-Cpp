61e4766da56b2158f422c70772991b39d58556d8
Nels Buhrley <nelsbuhrley@gmail.com>
2025-03-07 13:31:24 -0700

    hi

--- Week 2: Using Classes/Week 2: Using Classes/account.cpp
+++ Week 2: Using Classes/Week 2: Using Classes/account.cpp
@@ -4,5 +4,27 @@
 //
 //  Created by Nels Buhrley  on 3/7/25.
 //
+//#include <stdio.h>
+//#include <string>
+//class Account {
+//private:
+//    int accountID;
+//    std::string name;
+//    float accountBalance;
+//public:
+//    Account()
+//    void EstablishAccount()
+//    void DisplayInfo()
+//    void Deposit(float amount)
+//    void Withdrawl(float amount)
+//    Account& operator+= (float amount)
+//    Account& operator-= (float amount)
+//    std::ostream& display(std::ostream& out) const;
+//};
+
 
 #include "account.hpp"
+
+Account::Account()
+
+
--- Week 2: Using Classes/Week 2: Using Classes/account.hpp
+++ Week 2: Using Classes/Week 2: Using Classes/account.hpp
@@ -8,6 +8,23 @@
 #ifndef account_hpp
 #define account_hpp
 
-#include <stdio.h>
 
+#include <stdio.h>
+#include <string>
+class Account {
+private:
+    int accountID;
+    std::string name;
+    float accountBalance;
+public:
+    Account();
+    void EstablishAccount();
+    void DisplayInfo();
+    void Deposit(float amount);
+    void Withdrawl(float amount);
+    Account& operator+= (float amount);
+    Account& operator-= (float amount);
+    std::ostream& display(std::ostream& out) const;
+};
 #endif /* account_hpp */
+

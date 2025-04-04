467aa536314bc5672d2b960090a74aa50f711df2
Nels Buhrley <nelsbuhrley@gmail.com>
2025-04-03 19:20:23 -0600

    Finished C++ program

---
+++ Week 2: Using Classes/Week 2: Using Classes/Contact.cpp
@@ -0,0 +1,8 @@
+//
+//  Contact.cpp
+//  Week 2: Using Classes
+//
+//  Created by Nels Buhrley  on 3/31/25.
+//
+
+#include "Contact.hpp"
---
+++ Week 2: Using Classes/Week 2: Using Classes/Contact.hpp
@@ -0,0 +1,13 @@
+//
+//  Contact.hpp
+//  Week 2: Using Classes
+//
+//  Created by Nels Buhrley  on 3/31/25.
+//
+
+#ifndef Contact_hpp
+#define Contact_hpp
+
+#include <stdio.h>
+
+#endif /* Contact_hpp */
